2fec7002d9e8e050a096dc4646fa2346cde6760d
Sophia <phuongmai001@gmail.com>
2018-05-15 18:15:31 -0700

    add factorial problem

---
+++ e-recursion-factorial/factorial.js
@@ -0,0 +1,13 @@
+const factorial = (n, list={}) => {
+  if (n === 0) {
+    list[n] = 1;
+    return 1;
+  }
+  list[n] = n * factorial(n-1, list);
+  console.log(list);
+  return n * factorial(n-1, list);
+};
+
+console.log('factorial of 4 is ', factorial(4));
+console.log('factorial of 3 is ', factorial(3));
+console.log('factorial of 0 is ', factorial(0));
