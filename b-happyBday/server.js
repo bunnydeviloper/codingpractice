c6a41fd5d2eb74cf52665e8df2145742d5c4db3a
Sophia <phuongmai001@gmail.com>
2018-05-21 20:54:09 -0700

    move all html to separate repo for easy access

--- b-happyBday/server.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const app = express();
-app.use(express.static('public'));
-app.get('/', (req, res) => res.send());
-
-app.listen(13129, () => {
-  console.log('Running on port 13129, spajax.garagescript.org');
-});
