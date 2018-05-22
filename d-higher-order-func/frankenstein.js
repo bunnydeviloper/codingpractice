4203d71a60750a86f89828cf2a18f1744a831b69
Sophia <phuongmai001@gmail.com>
2018-05-21 19:05:11 -0700

    update frankenstein function

--- d-higher-order-func/frankenstein.js
+++ d-higher-order-func/frankenstein.js
@@ -14,3 +14,12 @@ console.log(frankenstein('sophia',
   e => e + e,	// action2: double word
   w => w.split('').reverse().join('')));  // action3: reverse word
 
+// recommended way:
+const compose = (x, f, g, h) => f(g(h(x)));
+const dupFront = (s) => s[0] + s;
+const doubleStr = (s) => s + s;
+const reverse = (s) => s.split('').reverse().join('');
+
+const franken2 = (x) => compose(x, reverse, dupFront, doubleStr);
+
+console.log(franken2('cal'));
