be7640e33f98732b29a9813dffd8c918e8a77961
Sophia Mai <phuongmai001@gmail.com>
2018-09-10 04:54:04 -0700

    add walk the tree function

--- f-tree/basicTree.js
+++ f-tree/basicTree.js
@@ -5,10 +5,18 @@ function Node(value, left, right) {
   this.right = right;
 }
 
-const myTree = new Node('Learn', new Node('JavaScript', null, null), new Node('and', new('Data',
+const myTree = new Node('Learn', new Node('JavaScript', null, null), new Node('and', new Node('Data',
   null, null), new Node('Structure', null, null)));
 
 console.log(myTree);
 
 // create a function that walks the tree from top to bottom, left to right (DFS?)
-functio
+function walk(tree, fn) {
+  if (tree) {
+    fn(tree.value);
+    walk(tree.left, fn);
+    walk(tree.right, fn);
+  }
+}
+
+walk(myTree, s => console.log(s)); // Learn JavaScript and Data Structure
