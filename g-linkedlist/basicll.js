6ded0b19c791fe7f4572f027ff5fbb64bdaa66b9
Sophia <phuongmai001@gmail.com>
2018-09-12 12:16:16 -0700

    change name from push to addToTail

--- g-linkedlist/basicll.js
+++ g-linkedlist/basicll.js
@@ -16,8 +16,8 @@ function SinglyLL() {
   this.head = null;
 }
 
-// add a node to a singly linked list
-SinglyLL.prototype.push = function (value) {
+// add a node to the end (tail) of a singly linked list
+SinglyLL.prototype.addToTail = function (value) {
   // first, create a node with the provided value
   const node = {
     value: value,
@@ -38,24 +38,31 @@ SinglyLL.prototype.push = function (value) {
   }
 };
 
-// test case for adding a node to singly linked list
+// test case for adding a node to the tail of a singly linked list
 const mysll = new SinglyLL()
-mysll.push('first one');
-mysll.push('second one');
-mysll.push('third one');
+mysll.addToTail('first one');
+mysll.addToTail('second one');
+mysll.addToTail('third one');
 console.dir(mysll, {depth: null});
 
 // remove a node from a singly linked list (note: there's no previous node)
 SinglyLL.prototype.remove = function(value) {
   let current = this.head;
 
-  // case1: head is the node we're looking for, simply change the head to the next one
+  // find the node we need to remove by comparing the value of current node with value-to-remove
   if (current.value === value) {
+    // case1: head is the node we're looking for, simply change the head to the next one
     this.head = current.next;
   } else {
     let previous = current;
 
     // case2: if the node is in the tail, then after removal, the beforeTail.next will become null
+    while (current.next) {
+      if (current.value === value) {
+        previous.next = current.next;
+        break;
+      }
+    }
     // case3: if the node is somewhere in middle, after removal, the middle.next will become current
   }
 };
