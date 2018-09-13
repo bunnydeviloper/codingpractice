106e87faf6a691fe89b78b67ee8d1b612969a9db
Sophia <phuongmai001@gmail.com>
2018-09-13 14:16:12 -0700

    update reverse fn

--- g-linkedlist/reverseSLL.js
+++ g-linkedlist/reverseSLL.js
@@ -8,3 +8,29 @@ function Node (value, next = null) {
   this.value = value;
   this.next = next;
 }
+
+// function to reverse a singly linked list
+function reverseSLL (sll) {
+  // if the linkedlist doesnt have any element, or just one head, return the same linkedlist
+  if (!sll.head || !sll.head.next) return sll;
+
+  // else, keep reversing pointers of two nodes at a time
+  let curr = sll.head;
+  let prev = new LinkedList();
+
+  while (curr) {
+    let save = curr.next; // save the next node
+    curr.next = prev; // reverse pointer
+
+    prev.next = curr; // increment previous to current node
+    curr.next = save; // increment current to the saved node above and repeat (or null if at the end)
+  }
+  return prev;
+}
+
+const mysll = new LinkedList();
+mysll.head = new Node(1, new Node(2, new Node(3)));
+console.dir(mysll, {depth: null});
+
+reverseSLL(mysll);
+console.dir(mysll, {depth: null});
