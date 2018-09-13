a4487ae66acbb0555c2b44d67531c2a98d2a779a
Sophia <phuongmai001@gmail.com>
2018-09-13 16:10:36 -0700

    update

--- g-linkedlist/reverseSLL.js
+++ g-linkedlist/reverseSLL.js
@@ -34,3 +34,38 @@ console.dir(mysll, {depth: null});
 
 reverseSLL(mysll);
 console.dir(mysll, {depth: null});
+
+
+const mysll2 = new LinkedList();
+mysll2.head = new Node(4, new Node(5, new Node(6)));
+console.dir(mysll2, {depth: null});
+
+// reverse a linked list with array
+function reverseSLLArray (sll) {
+  if (!sll.head || !sll.head.next) return sll;
+
+  let nodes = [];
+  current = sll.head;
+  while (current) {
+    // store all nodes into an array
+    nodes.push(current);
+    current = current.next;
+  }
+
+  const newLL = new LinkedList();
+  newLL.head = nodes.pop(); // make the last item in the nodes array to be the head
+  current = newLL.head;
+  let node = nodes.pop();
+  while (node) {
+    node.next = null; // to ensure the last node of newLL to be null
+    current.next = node;
+
+    current = current.next;
+    node = nodes.pop();
+  }
+  console.log('this is new LL ', newLL); // this is correct, but console.dir  is not right...
+  // console.dir(newLL, {depth: null});
+  return newLL;
+}
+reverseSLLArray(mysll2);
+console.dir(mysll2, {depth: null}); // ??? not sure why it's not working
