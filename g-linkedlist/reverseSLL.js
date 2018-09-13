6ce12f0eb9eb555df46d54976f8761dfc113147a
Sophia <phuongmai001@gmail.com>
2018-09-13 13:42:06 -0700

    add reverse file, initital

---
+++ g-linkedlist/reverseSLL.js
@@ -0,0 +1,10 @@
+// constructor function for Linked List
+function LinkedList() {
+  this.head = null;
+}
+
+// constructor function for Node
+function Node (value, next = null) {
+  this.value = value;
+  this.next = next;
+}
