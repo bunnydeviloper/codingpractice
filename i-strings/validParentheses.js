a3647fc8e8de46355827a9964c8ec36baae22204
Sophia Mai <phuongmai001@gmail.com>
2018-09-15 16:38:55 -0700

    add valid paren exercise with recursion

--- i-strings/validParentheses.js
+++ i-strings/validParentheses.js
@@ -1,56 +1,24 @@
-function matchingParen(s) {
-  let paren = 0;
+// same as matchingParen exercise, but use recursion
 
-  for (let i = 0; i < s.length; i ++) {
-    if (s.charAt(i) === "(") paren++;
-    if (s.charAt(i) === ")") paren--;
-    if (paren < 0) return false;
+function validParentheses(s, pairs = {"(":")", "[":"]", "{":"}"}, stack = [], i = 0) {
+  if (i === s.length) return stack.length === 0; // base case
+
+  // if the current character is an open bracket, push it on to the stack
+  if (Object.keys(pairs).includes(s.charAt(i))) {
+    stack.push(s.charAt(i));
   }
-
-  /* using while loop
-  let i = 0;
-  while (s.charAt(i)) {
-    if (s.charAt(i) === "(") paren++;
-    if (s.charAt(i) === ")") paren--;
-    i++;
-    if (paren < 0) {
-      break;
-    }
+  // if the current character is a closing bracket
+  if (Object.values(pairs).includes(s.charAt(i))) {
+    // and this closing bracket matches with the last begining bracket in our stack, then pop stack
+    let keyToCompare = Object.keys(pairs).find(k => pairs[k] === s.charAt(i));
+    if (stack[stack.length - 1] === keyToCompare) stack.pop();
+    // otherwise, return false
+    else return false;
   }
-  return (paren === 0) ? true : false;
-  */
-
-  return paren === 0;
+  return validParentheses(s, pairs, stack, i+1);
 }
 
-console.log(matchingParen(")I lov(e)")); // False
-console.log(matchingParen("(Hello)(")); // False
-console.log(matchingParen("Hi()")); // True
-
-// time: O(n)
-// space: O(1)
-
-// function moreParentheses(s, begin = ["(", "{", "["], end = [")", "}", "]"]) {
-function moreParentheses(s, pairs = {"(":")", "[":"]", "{":"}"}) {
-  let stack = [];
-
-  for (let i = 0; i < s.length; i++) {
-    // if the current character is an open bracket, push it on to the stack
-    if (Object.keys(pairs).includes(s.charAt(i))) stack.push(s.charAt(i));
-
-    // if the current character is a closing bracket
-    if (Object.values(pairs).includes(s.charAt(i))) {
-      // and this closing bracket matches with the last begining bracket in our stack, then pop stack
-      let keyToCompare = Object.keys(pairs).find(k => pairs[k] === s.charAt(i));
-      if (stack[stack.length - 1] === keyToCompare) stack.pop();
-      // otherwise, return false
-      else return false;
-    }
-  }
-  return stack.length === 0;
-}
-
-console.log(moreParentheses("[Love()]{}")); // True
-console.log(moreParentheses("]Love()]{}")); // False
-console.log(moreParentheses("[Love(]{)}")); // False
+console.log(validParentheses("[Love()]{}")); // True
+console.log(validParentheses("]Love()]{}")); // False
+console.log(validParentheses("[Love(]{)}")); // False
 
