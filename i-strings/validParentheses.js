7a11996cc73e2975ebea64804046a4fa74902566
Sophia Mai <phuongmai001@gmail.com>
2018-09-15 16:50:41 -0700

    add valid parentheses exercise

--- i-strings/validParentheses.js
+++ i-strings/validParentheses.js
@@ -1,24 +1,28 @@
 // same as matchingParen exercise, but use recursion
 
-function validParentheses(s, pairs = {"(":")", "[":"]", "{":"}"}, stack = [], i = 0) {
-  if (i === s.length) return stack.length === 0; // base case
+function validParentheses(s, pairs = {"(":")", "[":"]", "{":"}"}, stack = []) {
+  if (!s) return stack.length === 0; // base case
 
-  // if the current character is an open bracket, push it on to the stack
-  if (Object.keys(pairs).includes(s.charAt(i))) {
-    stack.push(s.charAt(i));
+  // if the first char of the current string is an open bracket, push it on to the stack
+  if (Object.keys(pairs).includes(s.charAt(0))) {
+    stack.push(s.charAt(0));
   }
-  // if the current character is a closing bracket
-  if (Object.values(pairs).includes(s.charAt(i))) {
+  // if the first char of the current string is a closing bracket
+  if (Object.values(pairs).includes(s.charAt(0))) {
     // and this closing bracket matches with the last begining bracket in our stack, then pop stack
-    let keyToCompare = Object.keys(pairs).find(k => pairs[k] === s.charAt(i));
+    let keyToCompare = Object.keys(pairs).find(k => pairs[k] === s.charAt(0));
     if (stack[stack.length - 1] === keyToCompare) stack.pop();
     // otherwise, return false
     else return false;
   }
-  return validParentheses(s, pairs, stack, i+1);
+
+  // each recursive call, we remove the first character of the original string
+  return validParentheses(s.substring(1), pairs, stack);
 }
 
-console.log(validParentheses("[Love()]{}")); // True
-console.log(validParentheses("]Love()]{}")); // False
-console.log(validParentheses("[Love(]{)}")); // False
+// time: O(n)
+// space: O(n)
 
+console.log("(x)[y](z) ([hello](world)[]): ", validParentheses("(x)[y](z) ([hello](world)[])")); // True
+console.log("( (] ([)]: ", validParentheses("( (] ([)]")); // False
+console.log("({foo)}: ", validParentheses("({foo)}")); // False
