5e214c46123658d7a6fb771b88c96a1873e5712d
Sophia Mai <phuongmai001@gmail.com>
2018-09-16 00:42:07 -0700

    update two Product function

--- h-arrays/twoProduct.js
+++ h-arrays/twoProduct.js
@@ -23,3 +23,40 @@ function twoProduct (array) {
 const test = [7, 0, -4, 5, 2, 3];
 console.log(twoProduct(test)); // 35
 
+// using array.sort() method
+function twoProductSort (array) {
+  array = array.sort(); // this will sort smallest to highest
+  return array[array.length-1] * array[array.length-2];
+}
+// time: O(n log n) b/c of the sort method
+
+console.log(twoProductSort(test)); // 35
+
+// using only one loop, but keep comparing highest number
+function twoProductOptimal (array) {
+  /*
+  const twoSum = (arr) => {
+    let highestInt = 0;
+    let nextHighestInt = 0;
+    for (let i=0; i<arr.length; i++) {
+      if (arr[i] > highestInt) {
+        nextHighestInt = highestInt;
+        highestInt = arr[i];
+      } else if (arr[i] > nextHighestInt) {
+        nextHighestInt = arr[i];
+      } else {
+        continue;
+      }
+    }
+    return highestInt * nextHighestInt;
+  }
+  // time: O(n) b/c we only loop once
+  */
+
+  let max = Math.max(...array);
+  array.splice(array.indexOf(max), 1); // remove the max element
+  return max * Math.max(...array);
+}
+// time: O(n) ? , b/c Math.max need to loop once to find the max value
+
+console.log(twoProductOptimal(test)); // 35
