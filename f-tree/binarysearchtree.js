b39dcd3e27248da449c45a18593f4644983ed66d
Sophia <phuongmai001@gmail.com>
2018-09-17 15:31:26 -0700

    update bst push fn

--- f-tree/binarysearchtree.js
+++ f-tree/binarysearchtree.js
@@ -14,7 +14,19 @@ function BST() {
 // note: BST characteristics: every node on the left is smaller than the value of the node
 // and the value on the right is higher than the value of the root
 BST.prototype.push = function(value) {
-  const root = this.root;
+  const newNode = new Node(value);
+  if (!this.root) return this.root = newNode;
+
+  let current = this.root;
+  while(current) {
+    if (value < current.value) {
+      if (!current.left) current.left = newNode;
+      else current = current.left;
+    } else {
+      if (!current.right) current.right = newNode;
+      else current = current.right;
+    }
+  }
 }
 
 const testBst = new BST();
@@ -24,3 +36,4 @@ testBst.push(9);
 testBst.push(7);
 testBst.push(5);
 testBst.push(0);
+console.dir(testBst, {depth: null});
