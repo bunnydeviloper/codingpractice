31eb19e8c8ede6fd08b2a0a430d039193d5d68d9
Sophia Mai <phuongmai001@gmail.com>
2018-09-18 16:29:53 -0700

    add remove dups exercise

---
+++ i-strings/removeDups.js
@@ -0,0 +1,7 @@
+// write a function to remove the duplicate letters in a string
+function removeDups(s) {
+  // TODO:
+}
+
+console.log(removeDups("Heeeelloooo")); // Helo
+console.log(removeDups("Google Search Engine")); // Gogle SarchEni
