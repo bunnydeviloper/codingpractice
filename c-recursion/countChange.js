0cc694f52d535e3b7a6860694a09693b33e3edf6
Sophia Mai <phuongmai001@gmail.com>
2018-09-19 22:52:48 -0700

    update coin change problem

--- c-recursion/countChange.js
+++ c-recursion/countChange.js
@@ -1,9 +1,25 @@
 // given coins {50, 25, 10, 5, 1}, how many ways are there of making changes
 
+
+// calculate the number of ways of represent n in a sum of k(s) integers
+function splitSum(total, coinsArray) {
+  if (total === 0) return 1;
+  // if (total < coinsArray[0] || coinsArray.length === 0) return 0;
+  console.log('coins array ', coinsArray);
+  if (coinsArray.length === 0 || total < coinsArray[0]) return 0;
+  return splitSum(total - coinsArray[0], coinsArray) + splitSum(total, coinsArray.splice(0, 1));
+}
+
+console.log(splitSum(8, [1, 3, 5])); // 8 (ways)
+console.log(splitSum(10, [2, 5])); // 2 (ways)
+console.log(splitSum(0, [2, 5])); // 0 (ways)
+console.log(splitSum(10, [])); // 0 (ways)
+
+/*
 const countChange = (amount, list) => {
   if (amount < 0 || list.length === 0) return 0;
   if (amount === 0) return 1;
-  return countChange(amount, list.slice(1)) + countChange(amount - list[0], list); 
+  return countChange(amount, list.slice(1)) + countChange(amount - list[0], list);
 
 };
 
@@ -11,3 +27,4 @@ console.log('Number of ways to split 10 cents: ', countChange(10, [10, 5, 1]));
 console.log('Number of ways to split 24 cents: ', countChange(24, [25, 10, 5, 1]));
 console.log('Number of ways to split 8 cents: ', countChange(8, [10, 5, 1]));
 console.log('Number of ways to split 30 cents: ', countChange(30, [25, 10, 5, 1]));
+*/
