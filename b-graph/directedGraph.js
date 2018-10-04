bd57d6ae13ed1ab7acd4d855bb1567863d77e065
Sophia Mai <phuongmai001@gmail.com>
2018-10-04 15:54:53 -0700

    update graph dfs function

--- b-graph/directedGraph.js
+++ b-graph/directedGraph.js
@@ -30,3 +30,37 @@ graph.addEdge('E', 'B');
 graph.addEdge('D', 'B');
 
 console.log(graph);
+
+// DFS on graph
+Graph.prototype.dfs = function() {
+  // get all vertexes of graph and save them as array of nodes
+  const nodes = Object.keys(this.adjList);
+  console.log('All nodes: ', nodes);
+  const visited = {}; // initialize visited object to make sure we don't visit any node twice
+
+  // loop through array of nodes and call dfsUtils function (below) on each node
+  for (let i = 0; i < nodes.length; i++) {
+    const node = nodes[i];
+    console.log('Each node: ', node);
+    this.dfsUtils(node, visited);
+  }
+};
+
+Graph.prototype.dfsUtils = function(vertex, visited) {
+  // only perform the function on new vertex (un-visited)
+  if (!visited[vertex]) {
+    visited[vertex] = true;
+    console.log('visited list: ', visited);
+
+    // loop through array of neighbors and call dfsUtils function on each neighbor (precaution step)
+    const neighbors = this.adjList[vertex];
+    console.log('current vertex(node): ', vertex, ' and its neighbors are: ', neighbors);
+    for (let i = 0; i < neighbors.length; i++) {
+      const neighbor = neighbors[i];
+      console.log('calling dfsUtils on neighbor: ', neighbor);
+      this.dfsUtils(neighbor, visited);
+    }
+  }
+};
+
+graph.dfs();
