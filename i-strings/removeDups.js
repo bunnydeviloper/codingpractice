1ead798ea5fe02c6c1fef6d90acdc638648409ba
Sophia Mai <phuongmai001@gmail.com>
2018-10-06 12:54:39 -0700

    update remove dups fn

--- i-strings/removeDups.js
+++ i-strings/removeDups.js
@@ -1,6 +1,6 @@
 // write a function to remove the duplicate letters in a string
 
-// Solution 1: using newStr or newArr data structure (if use array, will need to concat the characters)
+// Solution 1: using newStr data structure
 function removeDups(s) {
   let newStr = ""; // initialize a newStr variable
 
@@ -12,11 +12,19 @@ function removeDups(s) {
   return newStr;
 }
 
-// Solution 2: using recursion
-function removeDupsRecurse(s) {
-  //
-}
-
 console.log(removeDups("Heeeelloooo")); // Helo
 console.log(removeDups("google!!!")); // gole!
 console.log(removeDups("Search??? En gine")); // Search Engi
+
+// Solution 2: using array reduce method
+function removeDupsArr(s) {
+  let arr = s.split("");
+  return arr.reduce((acc, cur) => {
+    if (!acc.includes(cur)) acc.push(cur);
+    return acc;
+  }, []).join(""); // join() method needs a separator, otherwise default is ","
+}
+
+console.log('------------');
+console.log(removeDupsArr("Hiiiii Maiiiii")); // Hi Ma
+
