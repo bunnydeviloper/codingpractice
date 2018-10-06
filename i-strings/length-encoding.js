5c2b9590b9f61df805ee1979527284c61d7cf1ed
Sophia Mai <phuongmai001@gmail.com>
2018-10-06 15:14:04 -0700

    add encode decode exercise

---
+++ i-strings/length-encoding.js
@@ -0,0 +1,19 @@
+// take in a string, return an encoded string based on duplicate characters
+
+function lengthEncode (s) => {
+  let newStr = "";
+  return newStr;
+}
+
+console.log(lengthEncode("wwabbbc")); // w2a1b3c1
+console.log(lengthEncode("???abcdddddee")); // ?3a1b1c1d5e2
+
+// DECODE: take in an encoded string, and decypted it to the original string
+
+function lengthDecode (s) => {
+  let orgStr = "";
+  return orgStr;
+}
+
+console.log(lengthEncode("w2a1b3c1")); // wwabbbc
+console.log(lengthEncode("?3a1b1c1d5e2")); // ???abcdddddee
