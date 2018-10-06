e52abf6c48111679edac3af479e7f77428710d73
Sophia Mai <phuongmai001@gmail.com>
2018-10-06 15:56:12 -0700

    update encode fn

--- i-strings/length-encoding.js
+++ i-strings/length-encoding.js
@@ -1,14 +1,23 @@
 // take in a string, return an encoded string based on duplicate characters
 
 function lengthEncode (s) {
-  let count = 1;
-  let newStr = s.charAt(0) + count;
-  console.log(newStr);
+  let obj = {};
 
   for (let index in s) {
-    // todo
+    if (!obj[s.charAt(index)]) {
+      obj[s.charAt(index)] = 1;
+    } else {
+      obj[s.charAt(index)]++;
+    }
   }
-  return newStr;
+
+  console.log(obj);
+
+  return Object.keys(obj).reduce((accum, curr) => {
+    accum = accum + curr + obj[curr];
+    return accum;
+  }, "");
+
 }
 
 console.log(lengthEncode("wwabbbc")); // w2a1b3c1
@@ -21,5 +30,5 @@ function lengthDecode (s) {
   return orgStr;
 }
 
-console.log(lengthEncode("w2a1b3c1")); // wwabbbc
-console.log(lengthEncode("?3a1b1c1d5e2")); // ???abcdddddee
+// console.log(lengthEncode("w2a1b3c1")); // wwabbbc
+// console.log(lengthEncode("?3a1b1c1d5e2")); // ???abcdddddee
