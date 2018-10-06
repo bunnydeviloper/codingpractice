47d532268d6e786ca344c4e41792a713e2197c7e
Sophia Mai <phuongmai001@gmail.com>
2018-10-06 12:24:59 -0700

    update remove dups fn

--- i-strings/removeDups.js
+++ i-strings/removeDups.js
@@ -1,7 +1,20 @@
 // write a function to remove the duplicate letters in a string
 function removeDups(s) {
-  // TODO:
+  // Way 1: using newStr or newArr data structure
+  // initialize a newStr variable
+  let newStr = "";
+
+  // iterate through each character
+  for (let c in s) {
+    console.log(c);
+  // if the character doesn't exist in newStr, add to newStr
+  }
+
+  return newStr;
+
+  // Way 2: using recursion
 }
 
 console.log(removeDups("Heeeelloooo")); // Helo
+console.log(removeDups("google!!!")); // gole!
 console.log(removeDups("Google Search Engine")); // Gogle SarchEni
