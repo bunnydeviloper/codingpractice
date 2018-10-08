ff5bf68980ea4f5d484f39b0e94c8595c4119d90
Sophia Mai <phuongmai001@gmail.com>
2018-10-08 10:47:46 -0700

    update decode function

--- i-strings/length-encoding.js
+++ i-strings/length-encoding.js
@@ -27,25 +27,17 @@ console.log('-------------');
 // DECODE: take in an encoded string, and decypted it to the original string
 
 function lengthDecode (s) {
-  let orgStr = s.charAt(0);
+  let orgStr = '';
 
   for (let i = 1; i < s.length; i=i+2) {
     let counter = s.charAt(i);
-
-
-    while (counter > 1) {
-    console.log('counter', counter);
+    while (counter >= 1) {
       orgStr = orgStr + s.charAt(i-1);
       counter--;
-    }
-
-    if (counter === 1) {
-      orgStr = orgStr + s.charAt(i+1);
-      continue;
     }
   }
   return orgStr;
 }
 
 console.log(lengthDecode("w2a1b3c1")); // wwabbbc
-// console.log(lengthDecode("?3a1b1c1d5e2")); // ???abcdddddee
+console.log(lengthDecode("?3a1b1c1d5e2")); // ???abcdddddee
