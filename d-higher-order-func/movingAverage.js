7a444a7c17c52fe46cfaacb8df47f5c11eb4d8cc
Sophia Mai <phuongmai001@gmail.com>
2018-12-07 08:29:55 -0800

    im awake now, this exercise better be correct, haha...

--- d-higher-order-func/movingAverage.js
+++ d-higher-order-func/movingAverage.js
@@ -1,21 +1,48 @@
-// function takes one argument
+// function takes one argument, initialize with a base number
 // each time movingAverage.next(n) will return average number from current and all previous call
 
-function MovingAverage (base=0, counter=0) {
-  this.base = base;
-  this.counter = counter;
+function MovingAverage (base) {
+  // this.base = base; // for constructor, but essentially dont need
+
+  this.next = (function(base) {
+    let numerator = base;
+    let denominator;
+
+    (base === 0) ? denominator = 0 : denominator = 1;
+
+    return function(addMore) {
+
+      // prevent edge cases when user pass 0 into .next(), we don't increase denominator
+      if (addMore === 0) {
+        console.log(numerator + ' / ' +  denominator);
+        return numerator/denominator;
+      }
+
+      else {
+        numerator += addMore;
+        denominator++;
+
+        // prevent edge case when user pass negative num into .next() thus total sum of numerator is 0
+        if (numerator === 0) {
+          console.log('Error, total sum of all numbers is 0');
+          return;
+        } else {
+          console.log(numerator + ' / ' +  denominator);
+          return numerator/denominator;
+        }
+      }
+    }
+  })(base); // Self-Invoking Function
 }
 
-MovingAverage.prototype.next = function(n) {
-  // return this.base + n;
-  return (function(a) {
-  console.log(this.base + n + a);
-    return this.base + n + a;
-  })();
-};
+const testMovingAverageZeroBase = new MovingAverage(0);
+testMovingAverageZeroBase.next(2); // 2/1
 
-const testMovingAverage = new MovingAverage(2);
-testMovingAverage.next(4);
-testMovingAverage.next(1);
-// console.log(testMovingAverage.next(5));
-// console.log(testMovingAverage.next(9));
+const testMovingAverageRegular = new MovingAverage(2);
+testMovingAverageRegular.next(4); // 6/2
+testMovingAverageRegular.next(0); // 6/2
+testMovingAverageRegular.next(3); // 9/3
+
+const testMovinaAverageZeroNumerator = new MovingAverage(3);
+testMovinaAverageZeroNumerator.next(1); // 4/2
+testMovinaAverageZeroNumerator.next(-4); // Error
