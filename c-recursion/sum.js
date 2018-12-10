23203a61fab272c3fa01f6cc06efa7d506b821b9
Sophia Mai <phuongmai001@gmail.com>
2018-12-10 14:54:12 -0800

    add count fn as well

--- c-recursion/sum.js
+++ c-recursion/sum.js
@@ -1,10 +1,19 @@
-function sum(array) {
+function sum (array) {
     if (array.length === 1) return array[0];
     return array[array.length - 1] + sum(array.slice(0, array.length - 1));
 }
 
-let result = sum([1, 2, 3, 4, 5]);
-console.log(result);
+function count (array) {
+    if (array.length === 1) return 1;
+    return 1 + count(array.slice(0, array.length -1));
+}
 
-result = sum([2, 3, 4]);
-console.log(result);
+
+
+let testArray = [1, 2, 3, 4, 5];
+console.log('sum result: ', sum(testArray));
+console.log('count: ', count(testArray));
+
+testArray = [2, -3, 0];
+console.log('sum result: ', sum(testArray));
+console.log('count: ', count(testArray));
