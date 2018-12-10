69506c84a0b8fd77c91e9423dbae736d49118ce6
Sophia Mai <phuongmai001@gmail.com>
2018-12-10 15:04:21 -0800

    add max recursion

--- c-recursion/sum.js
+++ c-recursion/sum.js
@@ -8,12 +8,19 @@ function count (array) {
     return 1 + count(array.slice(0, array.length -1));
 }
 
-
+function max (array) {
+    if (array.length === 1) return array[0];
+    if (array[0] > array [1]) array.splice(1, 1);
+    else array.splice(0, 1);
+    return max(array);
+}
 
 let testArray = [1, 2, 3, 4, 5];
 console.log('sum result: ', sum(testArray));
 console.log('count: ', count(testArray));
+console.log('max: ', max(testArray));
 
 testArray = [2, -3, 0];
 console.log('sum result: ', sum(testArray));
 console.log('count: ', count(testArray));
+console.log('max: ', max(testArray));
