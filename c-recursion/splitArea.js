03e121671831557672232fac77440a88bc23f9a2
Sophia Mai <phuongmai001@gmail.com>
2018-12-10 23:54:36 -0800

    update splitArea fn

--- c-recursion/splitArea.js
+++ c-recursion/splitArea.js
@@ -2,12 +2,18 @@
 // return a list of area of the largest squares you could make out of those panels
 // eg: area(15324) will return [15129, 169, 25, 1]
 
-function area(solarPanels) {
-  let list = [];
-
-  console.log('Result: ', list);
-  return list;
+function splitArea(solarPanels, list = []) {
+  const square = Math.pow(Math.floor(Math.sqrt(solarPanels)), 2);
+  if (solarPanels === 0) {
+    console.log('Final result: ', list);
+    return list;
+  }
+  else if (solarPanels === 1) { list.push(solarPanels); }
+  else { list.push(square); }
+  return splitArea(solarPanels - square, list);
 }
 
-area(12); // [9, 1, 1, 1]
-area(15324);
+splitArea(12); // [9, 1, 1, 1]
+splitArea(15324); // [15129, 169, 25, 1]
+splitArea(1); // [1]
+splitArea(0); // []
