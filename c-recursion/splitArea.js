1af93fc92e3ce79ac17190cba1cbc8040d252ee2
Sophia Mai <phuongmai001@gmail.com>
2018-12-10 23:32:50 -0800

    initilize counting area of solar panels

---
+++ c-recursion/splitArea.js
@@ -0,0 +1,13 @@
+// write a fn that takes in a number to represent total solar panels you have
+// return a list of area of the largest squares you could make out of those panels
+// eg: area(15324) will return [15129, 169, 25, 1]
+
+function area(solarPanels) {
+  let list = [];
+
+  console.log('Result: ', list);
+  return list;
+}
+
+area(12); // [9, 1, 1, 1]
+area(15324);
