70c0e05617c17b2f4ac0aa18fae7894a7bce5575
Sophia Mai <phuongmai001@gmail.com>
2018-12-13 15:34:00 -0800

    add Euclidian algorithm

--- c-recursion/divideField-GCD.js
+++ c-recursion/divideField-GCD.js
@@ -1,5 +1,5 @@
+// similar to finding the greatest common divisor
 function divideField (height, width) {
-    // similar to finding the greatest common divisor
     if (height === width) return height;
     if (height > width) {
         return divideField(height - width, width); 
@@ -11,3 +11,22 @@ function divideField (height, width) {
 console.log(divideField(3, 12)); // 3
 console.log(divideField(5, 12)); // 1
 console.log(divideField(1640, 680)); // 40
+
+// GCD is also Euclid Algorithm (HCF - highest common factor)
+// and also called prime factorization problem
+function euclidianAlgorithm (num1, num2) {
+    if (num1 >= num2) { // num1 is max
+        if (num1 % num2 === 0) return num2;
+        else return euclidianAlgorithm(num1 - num2, num2);
+    } else return euclidianAlgorithm(num2, num1);
+
+    /* same as:
+    } else {
+        if (num2 % num1 === 0) return num1;
+        else return euclidianAlgorithm(num2 - num1, num1);
+    }
+    */
+}
+
+console.log('HCF: ', euclidianAlgorithm(12345, 285)); // 15
+console.log('HCF: ', euclidianAlgorithm(12345, 12344)); // 1
