1f24ee29d30fa55ae7620d93c2ce8cbd26fdb869
Sophia Mai <phuongmai001@gmail.com>
2018-12-14 10:27:41 -0800

    'CLEAN clean up code'

--- h-arrays/arrayProduct.js
+++ h-arrays/arrayProduct.js
@@ -13,10 +13,7 @@ function computeProduct (array) {
     return array.reduce((curr, accum) => accum * curr);
 }
 
-const testInput = [1, 2, 3, 4, 5];
-const testOutput =  [120, 60, 40, 30, 24];
-
 console.log(arrayProduct([3, 2, 1])); // [2, 3, 6]
-
+console.log(arrayProduct([1, 2, 3, 4, 5])); // [120, 60, 40, 30, 24]
 
 // TODO: Follow-up: what if you can't use division?
