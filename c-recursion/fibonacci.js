d5b660f17b16ba82eec9336ab5ad360adbb9bca7
Sophia Mai <phuongmai001@gmail.com>
2018-12-23 02:06:07 -0800

    update fib

--- c-recursion/fibonacci.js
+++ c-recursion/fibonacci.js
@@ -1,24 +1,34 @@
-// Time: O(2^n) | Space: O(n)
-const getNthFib = (n) => {
-  if (n < 2) return n;
-  return getNthFib(n-1) + getNthFib(n-2);
+// FIB: 0, 1, 1, 2, 3, 5, 8, 13, 21, 34...
 
-  /*
-  if (n === 1) return 1;
-  else if (n === 0) return 0;
+// Simple Fibbo:
+const fib = (n) => {
+  if (n < 2) return n; // same as: if n===1 return 1; if n===0 return 0
+  return fib(n-1) + fib(n-2);
+}
+console.log(fib(5)); // 5
+console.log(fib(0)); // 0
+console.log(fib(6)); // 8
+console.log(fib(2)); // 1
+
+// note: if you start counting first elem as 1...
+const getNthFib = (n) => {
+  if (n === 2) return 1;
+  else if (n === 1) return 0;
   else return getNthFib(n-1) + getNthFib(n-2);
-  */
 };
 
-// FIB: 0, 1, 1, 2, 3, 5, 8
-console.log(getNthFib(5)); // 5
-console.log(getNthFib(0)); // 0
-console.log(getNthFib(6)); // 8
+console.log(getNthFib(5)); // 3
+console.log(getNthFib(6)); // 5
 console.log(getNthFib(2)); // 1
+// console.log(getNthFib(0)); // can't use
+
+// Both fib() and getNthFib() analysis: Time: O(2^n) | Space: O(n)
+
+// Improve: Time: O(n) | Space: O(n)
+function getNthFibIMP (n, memoize = {1: 0, 2: 1}) {
+  // if  
+}
 
 
-// Time: O(n) | Space: O(n)
-
-
-// Time: O(n) | Space: O(1)
+// Improve2: Time: O(n) | Space: O(1)
 
