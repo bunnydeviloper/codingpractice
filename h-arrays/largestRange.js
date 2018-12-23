e986fb0a90c7e7c5cf50f0ef5298b26c974f3e8f
Sophia Mai <phuongmai001@gmail.com>
2018-12-23 11:07:05 -0800

    add largest range prompt

---
+++ h-arrays/largestRange.js
@@ -0,0 +1,14 @@
+/*
+Write a function that takes in an array of unsorted integers, returns an array of length 2
+output array represents the largest range of numbers contained in input array
+A range is defined as a set of numbers that come right after each other
+(Assume there will only be one largest range)
+*/
+
+function largestRange(array) {
+  // Write your code here.
+}
+
+const input = [1, 11, 3, 0, 15, 5, 2, 4, 10, 7, 12, 6];
+console.log(largestRange(input)); // [0, 7]
+// [0, 1, 2, 3, 4, 5, 6, 7,\\ 10, 11, 12, 15]
