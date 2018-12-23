8f61914429d5aeec0bf5b861b9eed16e969d17b0
Sophia Mai <phuongmai001@gmail.com>
2018-12-23 10:11:17 -0800

    add another improve fn which can save more on space

--- c-recursion/fibonacci.js
+++ c-recursion/fibonacci.js
@@ -36,4 +36,19 @@ console.log('improve: ', getNthFibIMP(7)); // 8
 console.log('improve: ', getNthFibIMP(2)); // 1
 
 // Improve2: Time: O(n) | Space: O(1)
+function getNthFibSaveSpace (n) {
+  const lastTwo = [0, 1];
+  let counter = 3;
 
+  while (counter <= n) {
+    const nextFib = lastTwo[0] + lastTwo[1];
+    lastTwo[0] = lastTwo[1];
+    lastTwo[1] = nextFib;
+    counter++;
+  }
+
+  return (n > 1) ? lastTwo[1] : lastTwo[0];
+}
+
+console.log('save space: ', getNthFibSaveSpace(7)); // 8
+console.log('save space: ', getNthFibSaveSpace(8)); // 13
