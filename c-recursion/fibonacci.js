7d51e8179eaec231d36492927b37be8aef185ba8
Sophia Mai <phuongmai001@gmail.com>
2018-12-23 01:53:23 -0800

    update fibbo fn

--- c-recursion/fibonacci.js
+++ c-recursion/fibonacci.js
@@ -1,10 +1,24 @@
-const fib = (n) => {
+// Time: O(2^n) | Space: O(n)
+const getNthFib = (n) => {
   if (n < 2) return n;
-  return fib(n-1) + fib(n-2);
+  return getNthFib(n-1) + getNthFib(n-2);
+
+  /*
+  if (n === 1) return 1;
+  else if (n === 0) return 0;
+  else return getNthFib(n-1) + getNthFib(n-2);
+  */
 };
 
-console.log(fib(5));
-console.log(fib(0));
-console.log(fib(6));
-console.log(fib(2));
+// FIB: 0, 1, 1, 2, 3, 5, 8
+console.log(getNthFib(5)); // 5
+console.log(getNthFib(0)); // 0
+console.log(getNthFib(6)); // 8
+console.log(getNthFib(2)); // 1
+
+
+// Time: O(n) | Space: O(n)
+
+
+// Time: O(n) | Space: O(1)
 
