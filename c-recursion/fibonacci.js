0aa186d57fc8f6385c470b9de37aa261e4ce8023
Sophia Mai <phuongmai001@gmail.com>
2018-12-23 10:10:06 -0800

    update testing console.log

--- c-recursion/fibonacci.js
+++ c-recursion/fibonacci.js
@@ -5,10 +5,12 @@ const fib = (n) => {
   if (n < 2) return n; // same as: if n===1 return 1; if n===0 return 0
   return fib(n-1) + fib(n-2);
 }
-console.log(fib(5)); // 5
-console.log(fib(0)); // 0
-console.log(fib(6)); // 8
-console.log(fib(2)); // 1
+console.log('simple: ', fib(5)); // 5
+console.log('simple: ', fib(0)); // 0
+console.log('simple: ', fib(6)); // 8
+console.log('simple: ', fib(2)); // 1
+
+// Both fib() and getNthFib() analysis: Time: O(2^n) | Space: O(n)
 
 // note: if you start counting first elem as 1...
 const getNthFib = (n) => {
