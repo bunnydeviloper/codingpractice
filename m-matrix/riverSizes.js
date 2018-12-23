926e6edd1bb396bd86dfa9c97d94b039a4485b9a
Sophia Mai <phuongmai001@gmail.com>
2018-12-22 23:55:05 -0800

    add riversize exercise

---
+++ m-matrix/riverSizes.js
@@ -0,0 +1,24 @@
+/*
+Given a 2D array (matrix) (potentially unequal h x w)
+Each 0 represents land, each 1 represents part of a river.
+A river consists of any number of 1s that are either horizontally or vertically adjacent
+Write a function that returns an array of the sizes of all rivers
+*/
+
+/*
+Sample Input:
+[
+  [1, 0, 0, 1, 0],
+  [1, 0, 1, 0, 0],
+  [0, 0, 1, 0, 1],
+  [1, 0, 1, 0, 1],
+  [1, 0, 1, 1, 0]
+]
+Sample Output: (note the sizes do not need to be in any particular order)
+[1, 2, 2, 2, 5]
+*/
+
+function riverSizes(matrix) {
+  // Write your code here.
+}
+
