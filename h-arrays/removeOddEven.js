f9332d17df17b943bc08ec0abff1c3ec743a69d3
Sophia Mai <phuongmai001@gmail.com>
2018-12-25 16:21:14 -0800

    update removeOdds removeEvens fn

--- h-arrays/removeOddEven.js
+++ h-arrays/removeOddEven.js
@@ -1,11 +1,18 @@
-// use array.splice, note: array.splice will modify the original array
+// use array.splice and array.slice
 
 function removeOdds (array) {
+  for (let i = array.length; i >= 0; i--) {
+    if (array[i] % 2) array.splice(i, 1);
+  }
   return array;
 }
 
+// note: since splice method modifies the original array
+// we have to make sure to loop backward
+// otherwise, if we loop forward, i will be miscounted
+
 function removeEvens (array) {
-  return array;
+  return array.filter(e => e % 2 !== 0); // alternative
 }
 
 const testInput = [1, 2, 3, 4, 5, 6, 9, 11, 12, 14, 15, 20];
