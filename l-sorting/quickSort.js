824f3d9d4f4210e7273b35e06139dc2273d13c1c
Sophia Mai <phuongmai001@gmail.com>
2018-12-28 15:00:31 -0800

    add runtime analysis

--- l-sorting/quickSort.js
+++ l-sorting/quickSort.js
@@ -14,5 +14,14 @@ function quickSort (array) {
 
 }
 
+// Time: Average case: O(nlogn), Worst case: O(n^2)
+// to create 'lesser' and greater: O(n) + O(n) = O(2n) => O(n)
+// each time you call recursion => increase call stack => O(n)
+// total = O(n) * O(n) = O(n^2)
+// interpret: total time takes to complete each level * the height of the call stack
+
+// suppose array is already sorted, and you pick the middle element as pivot (best case) => call stack size will be O(log(n))
+// total = O(n) * O(log(n)) = O(nlogn)
+
 const testInput = [10, 3, 0, 4, 8, 15];
 console.log(quickSort(testInput)); // [0, 3, 4, 8, 10, 15]
