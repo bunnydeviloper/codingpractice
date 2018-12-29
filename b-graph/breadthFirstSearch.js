80083c91ae3f5b2c28a1cd0b7100a2d2fe8757b1
Sophia Mai <phuongmai001@gmail.com>
2018-12-28 17:36:09 -0800

    update findmangoseller fn

--- b-graph/breadthFirstSearch.js
+++ b-graph/breadthFirstSearch.js
@@ -1,7 +1,7 @@
 // make a graph of your friends network
 const myNetwork = {};
 
-myNetwork['me'] = ['hg', 'sophie', 'trang']; // 3 close friends (1st degree connections)
+myNetwork['you'] = ['hg', 'sophie', 'trang']; // 3 close friends (1st degree connections)
 myNetwork['hg'] = ['khoa']; // friends of friends (2nd degree connections)
 myNetwork['sophie'] = ['caroline', 'steph', 'mimi']; // 2nd degree
 myNetwork['trang'] = ['thomas', 'mike'] // 2nd degree
@@ -12,12 +12,34 @@ myNetwork['mimi'] = []; // undirected graph, so mimi doesn't point back to 'khoa
 myNetwork['thomas'] = [];
 myNetwork['mike'] = [];
 
-console.log(myNetwork);
-
-// use enqueue and dequeue
+// console.log(myNetwork);
 
 // try to find if anyone in your network sells mango
 // (same as: is there a path from A (you) to B (mango seller)
+function findMangoSellerBFS(graph) {
+  let searchQueue = graph['you']; // use enqueue and dequeue
+
+  while (searchQueue.length > 0) {
+    const person = searchQueue.shift();
+    if (isMangoSeller(person)) {
+      console.log('Found one mango seller name: ', person);
+      return true;
+    } else {
+      // if person is not mango seller, we add his/her friends to the searchQueue
+      graph[person].forEach(e => searchQueue.push(e));
+    }
+  }
+  console.log('Cannot find mango seller');
+  return false;
+}
+
+// note: if a person's name contains 'm', they do sell mangos
+function isMangoSeller(name) {
+  if (name.includes('m')) return true;
+  return false;
+}
+
+findMangoSellerBFS(myNetwork); // true
 
 // who is the closest mango seller
 // what is the shortest path from A to B?
