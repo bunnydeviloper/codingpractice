4135c2837f3b600847910cf367cb1c78477551f2
Sophia Mai <phuongmai001@gmail.com>
2018-12-31 13:35:47 -0800

    add more test cases

--- b-graph/dijkstraPractice.js
+++ b-graph/dijkstraPractice.js
@@ -68,3 +68,29 @@ console.log(result);
 // step2: init var inside dijkstra fn, identify helper fns, write outside
 // step3: write while loop inside dijkstra, update costObj + parentObj each iteration
 // step4: once while loop finished, write optimalResult helper fn
+
+const testInput1 = {
+  'start': { 'A': 5, 'C': 2 },
+  'A': { 'B': 4, 'D': 2 },
+  'B': { 'D': 6, 'finish': 3 },
+  'C': { 'A': 8, 'D': 7 },
+  'D': { 'finish': 1 },
+  'finish': {}
+}
+const testInput2 = { // circular graph, avoid the cycle
+  'start': { 'A': 10 },
+  'A': { 'B': 20 },
+  'B': { 'C': 1, 'finish': 30 },
+  'C': { 'A': 1 },
+  'finish': {}
+}
+const testInput3 = { // graph w/ -weight, dijkstra won't give optimalPath
+  'start': { 'A': 2, 'B': 2 },
+  'A': { 'C': 2, 'finish': 2 },
+  'B': { 'A': 2 },
+  'C': { 'B': -1, 'finish': 2 },
+  'finish': {}
+}
+console.log('testinput1 result: ', dijkstra(testInput1));
+console.log('testinput2 result: ', dijkstra(testInput2));
+console.log('testinput3 result: ', dijkstra(testInput3)); // expect error
