49f9800594768678c56a85afb01543ff79353244
Sophia Mai <phuongmai001@gmail.com>
2018-12-31 12:23:14 -0800

    update lowestCostNode fn

--- b-graph/dijkstraPractice.js
+++ b-graph/dijkstraPractice.js
@@ -9,7 +9,14 @@ const myGraph = {
 };
 
 function lowestCostNode (costObj, processedArr) {
-  //
+  return Object.keys(costObj).reduce((accum, currNode) => {
+    if (accum === null || costObj[currNode] < costObj[accum]) {
+      if (!processedArr.includes(currNode)) {
+        accum = currNode;
+      }
+    }
+    return accum;
+  }, null);
 }
 
 function createParentObj (graph) {
@@ -20,12 +27,23 @@ function createParentObj (graph) {
   return parentObj;
 }
 
+function optimalResult (parentObj) {
+  let optimalPathArr = [];
+  return optimalPathArr;
+}
+
 function dijkstra (graph) {
   const costObj = { ...graph.start, ...{'finish': Infinity} };
   const processedArr = [];
   const parentObj = createParentObj(graph);
 
-  console.log(parentObj);
+  let lowest = lowestCostNode(costObj, processedArr);
+  console.log(lowest);
 }
 
 dijkstra(myGraph);
+
+// step1: create graph, init dijkstra fn
+// step2: init var, identify helper fns
+// step3: write while loop inside dijkstra
+// step4: write optimalResult helper fn
