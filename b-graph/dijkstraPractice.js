2e24ada8001e1de160b14ca7f20dc13afcb31623
Sophia Mai <phuongmai001@gmail.com>
2018-12-31 12:13:47 -0800

    practice dijkstra part 1

---
+++ b-graph/dijkstraPractice.js
@@ -0,0 +1,27 @@
+// practice writing dijkstra fn
+// input: graph,  output: optimal path and total distance cost
+
+const myGraph = {
+  'start': {'A': 6, 'B': 2},
+  'A': {'finish': 1},
+  'B': {'A': 3, 'finish': 5},
+  'finish': {}
+};
+
+function lowestCostNode (costObj, processedArr) {
+ //
+}
+
+function createParentObj (graph) {
+  //
+}
+
+function dijkstra (graph) {
+  const costObj = { ...graph.start, ...{'finish': Infinity} };
+  const processedArr = [];
+  const parentObj = createParentObj(graph);
+
+  console.log(parentObj);
+}
+
+dijkstra(myGraph);
