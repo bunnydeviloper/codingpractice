ba9fcd28e2cc611ebdf10b9119c9f92cb24ec001
Sophia Mai <phuongmai001@gmail.com>
2019-01-03 14:57:52 -0800

    update dp solution and commnets

--- stockMaxProfit-DP.js
+++ stockMaxProfit-DP.js
@@ -28,17 +28,31 @@ function maxProfitWithKTransactions(prices, k) {
 
     for (let dayIndex = 0; dayIndex < prices.length; dayIndex++) {
       if (tIndex === 0 || dayIndex === 0) {
-        profitMatrix[tIndex][dayIndex] = 0;
+        // top row (0 transaction to start) and left-most row (first stock to start)
+        // values are always 0 b/c you have to buy a stock first to make a sell->profit
+        profitMatrix[tIndex][dayIndex] = 0; // 0 profit
+
       } else {
+        // calculate maxThusFar so that we can calculate current cell's value
         maxThusFar = Math.max(maxThusFar, profitMatrix[tIndex-1][dayIndex-1] - prices[dayIndex-1]);
         console.log('maxThusfar: ', maxThusFar);
+
+        // current cell value is Math.max(profit-of-prev-day OR today-selling-price-plus-maxThusFar)
         profitMatrix[tIndex][dayIndex] = Math.max(profitMatrix[tIndex][dayIndex-1], prices[dayIndex] + maxThusFar);
+
         console.log('profitMatrix so far: \n', profitMatrix);
       }
     }
   }
-  return profitMatrix[k][prices.length-1]; // return the value of last cell
+  return profitMatrix[k][prices.length-1]; // DP grid result always the value of last cell in matrix
 }
 
 const result = maxProfitWithKTransactions(inputPrices, 2);
 console.log(result); // 93
+
+// NOTE: maxThusFar is current-maxThusFar OR
+// max-profit-of-any-previous-day-(w-1-less-transaction)-minus-that-day's-selling-price
+// because you're gaining profit for that day, but also buy that same stock to sell today
+
+// in a sense: maxThusFar is the maximum trade-off (lost) from buying particular day's stock
+// at first, maxThusFar will be negative, but slowly rise to positive when you make more $$
