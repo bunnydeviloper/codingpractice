3f619b3eee6aa6287c326373d3572a4015e5ec8b
Sophia Mai <phuongmai001@gmail.com>
2019-01-02 19:53:01 -0800

    add makeGrid fn

--- a-ADVANCE/longestCommonSub.js
+++ a-ADVANCE/longestCommonSub.js
@@ -1,11 +1,21 @@
 // longest common substring
 
-function longestCommonSubstring (word1, word2) {
-  // return
+function makeGrid (string1, string2) {
+  let grid = [];
+  for (let i = 0; i < string1.length; i++) {
+    grid[i] = []; // set up inner array
+    for (let j = 0; j < string2.length; j++) {
+      // grid[i][j] = string2.charAt(j);
+      grid[i][j] = 0;
+    }
+  }
+  return grid;
 }
 
+// makeGrid('hi', 'twice');
+
 // longest common subsequence
-function longestCommonSubsequence (word1, word2) {
+function longestCommonSubsequence (mistypedWord, guessedWord) {
   // return
 }
 
