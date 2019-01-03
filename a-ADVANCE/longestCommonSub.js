7e8914f3ece7afe5dc5dc8604843b8739f2fd9d2
Sophia Mai <phuongmai001@gmail.com>
2019-01-02 19:53:15 -0800

    update longest common substring

--- a-ADVANCE/longestCommonSub.js
+++ a-ADVANCE/longestCommonSub.js
@@ -14,6 +14,26 @@ function makeGrid (string1, string2) {
 
 // makeGrid('hi', 'twice');
 
+function longestCommonSubstring (mistypedWord, guessedWord) {
+  let grid = makeGrid(mistypedWord, guessedWord);
+
+  for (let i = 0; i < mistypedWord.length; i++) {
+    for (let j = 0; j < guessedWord.length; j++) {
+      if (mistypedWord[i] === guessedWord[j]) {
+      // console.log('grid: ', grid[i-1]);
+        if (i === 0) grid[i][j] = 1;
+        else grid[i][j] = grid[i-1][j-1] + 1;
+      }
+      else grid[i][j] = 0;
+    }
+  }
+
+  console.log(grid);
+  return grid;
+}
+
+longestCommonSubstring('hish', 'fish');
+
 // longest common subsequence
 function longestCommonSubsequence (mistypedWord, guessedWord) {
   // return
