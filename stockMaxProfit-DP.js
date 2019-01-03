3bfafcb2db54ac341b361d1f6935a92fbf7c0c36
Sophia Mai <phuongmai001@gmail.com>
2019-01-03 12:36:20 -0800

    add helper makegrid fn

--- stockMaxProfit-DP.js
+++ stockMaxProfit-DP.js
@@ -12,11 +12,30 @@ only hold 1 share of the stock at a time: in other words, you cannot
 buy more than 1 share of the stock on any given day, and you cannot
 buy a share of the stock if you are still holding another share.
 
-Sample input: [5, 11, 3, 50, 69, 90], 2
+Sample input: [5, 11, 3, 50, 60, 90], 2
 Sample output: 93 (buy: 5, sell: 11, buy: 3, sell: 90)
 */
 
+function makeGrid(prices, k) {
+  let result = [];
+
+  for (let i = 0; i < k; i++) {
+    result.push([]);
+    for (let j = 0; j < prices.length; j++) {
+      result[i][j] = 0;
+    }
+  }
+  // console.log(result);
+  return result;
+}
+
+const inputPrices = [5, 11, 3, 50, 60, 90];
+makeGrid(inputPrices, 3);
+
 function maxProfitWithKTransactions(prices, k) {
-  // code
+  if (prices.length === 0) return 0;
+
+  // make a 2D array for our prices and k
+  let profitMatrix = makeGrid(prices, k+1);
 }
 
