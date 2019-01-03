12b17c532e969bc87c05cd1030a60331fb4bc639
Sophia Mai <phuongmai001@gmail.com>
2019-01-02 19:17:53 -0800

    init longest common sub exercise

---
+++ a-ADVANCE/longestCommonSub.js
@@ -0,0 +1,12 @@
+// longest common substring
+
+function longestCommonSubstring (word1, word2) {
+  // return
+}
+
+// longest common subsequence
+function longestCommonSubsequence (word1, word2) {
+  // return
+}
+
+
