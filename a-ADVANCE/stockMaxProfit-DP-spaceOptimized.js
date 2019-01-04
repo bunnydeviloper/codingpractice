949d73984a83200c3a167e8f5ae38302e6e872a6
Sophia Mai <phuongmai001@gmail.com>
2019-01-03 19:19:25 -0800

    add inner loop

--- a-ADVANCE/stockMaxProfit-DP-spaceOptimized.js
+++ a-ADVANCE/stockMaxProfit-DP-spaceOptimized.js
@@ -8,6 +8,22 @@ function maxProfit (prices, k) {
   let current = oddRow;
   let previous = evenRow;
 
+  for (let t = 1; t <=k; t++) {
+    let maxThusFar = -Infinity;
+
+    if (t % 2 === 1) { // odd
+      current = oddRow;
+      previous = evenRow;
+    } else { // even
+      current = evenRow;
+      previous = oddRow;
+    }
+
+  }
+  // console.log(oddRow);
+  // console.log(evenRow);
+
+  return (k % 2 === 0) ? evenRow[evenRow.length-1] : oddRow[oddRow.length-1];
 }
 
 let input = [2, 10, 5, 20, 30, 15];
