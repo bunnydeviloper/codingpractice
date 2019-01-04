a653fe97510b5a6ef2dce3579221d72ba1758763
Sophia Mai <phuongmai001@gmail.com>
2019-01-03 18:39:38 -0800

    init more optimized version

---
+++ a-ADVANCE/stockMaxProfit-DP-spaceOptimized.js
@@ -0,0 +1,13 @@
+// O(nk) time | O(n) space
+
+function maxProfit (prices, k) {
+  // return
+}
+
+const input = [2, 10, 5, 20, 30, 15];
+const result = maxProfit(input, 2);
+console.log(result);
+
+const input2 = [5, 11, 3, 50, 60, 90];
+result = maxProfit(input2, 2);
+console.log(result);
