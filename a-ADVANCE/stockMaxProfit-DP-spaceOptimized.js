b7a2a7277671163480c050830ced4997dfab0e75
Sophia Mai <phuongmai001@gmail.com>
2019-01-03 19:18:49 -0800

    start some pointer variables

--- a-ADVANCE/stockMaxProfit-DP-spaceOptimized.js
+++ a-ADVANCE/stockMaxProfit-DP-spaceOptimized.js
@@ -1,13 +1,19 @@
 // O(nk) time | O(n) space
 
 function maxProfit (prices, k) {
-  // return
+  if (prices.length === 0) return 0;
+
+  let oddRow = prices.map(e => e = 0);
+  let evenRow = prices.map(e => e = 0);
+  let current = oddRow;
+  let previous = evenRow;
+
 }
 
-const input = [2, 10, 5, 20, 30, 15];
-const result = maxProfit(input, 2);
-console.log(result);
+let input = [2, 10, 5, 20, 30, 15];
+let result = maxProfit(input, 2);
+console.log('result: ', result);
 
-const input2 = [5, 11, 3, 50, 60, 90];
-result = maxProfit(input2, 2);
-console.log(result);
+input = [5, 11, 3, 50, 60, 90];
+result = maxProfit(input, 2);
+console.log('result: ', result);
