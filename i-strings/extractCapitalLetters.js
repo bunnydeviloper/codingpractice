4d83287920b8939b1223a79b99610e698bbe7b07
Sophia Mai <phuongmai001@gmail.com>
2019-01-04 16:32:13 -0800

    rename file for better naming convention

--- i-strings/extractCapitalLetters.js
+++ i-strings/extractCapitalLetters.js
@@ -1,5 +1,5 @@
 // print all capital letters from a given string
-const capitalLetters = (s) => {
+const extractCapitalLetters = (s) => {
   let str = "";
   for (let i=0; i < s.length; i++) {
     if (s.charAt(i) === s.charAt(i).toUpperCase()
@@ -13,5 +13,5 @@ const capitalLetters = (s) => {
 const test = "Hello Elsa, Let it go, Let !! gO";
 const test2 = "Honey OhNora Enqueue todaY";
 
-console.log(capitalLetters(test)); // HELLO
-console.log(capitalLetters(test2)); // HONEY
+console.log(extractCapitalLetters(test)); // HELLO
+console.log(extractCapitalLetters(test2)); // HONEY
