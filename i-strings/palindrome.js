a7d44b61d43473ab33d11a3f721081bfdd4ac00b
Sophia Mai <phuongmai001@gmail.com>
2019-01-04 16:20:17 -0800

    update palindrome fn

--- i-strings/palindrome.js
+++ i-strings/palindrome.js
@@ -1,18 +1,13 @@
 // write a fn to check if input string is a palindrome
 // Easy version: reverse everything, ignore spaces
 function palindrome (s) {
-  let array = [];
-  s = s.toLowerCase();
-  for (let i=0; i < s.length; i++) {
-    if (s.charAt(i) !== " ") array.push(s.charAt(i));
-  }
-
-  console.log(array);
-  //
+  s = s.toLowerCase().replace(/\s/g, ''); // regrex remove spaces
+  return s === s.split('').reverse().join('');
 }
 
 console.log(palindrome("no x in nixon")); // true
-console.log(palindrome("race car")); // true
+console.log(palindrome("race cr")); // false
 console.log(palindrome("Amor Roma")); // true
 
-// Hard version: ignore capitalization, ignore punctuation: ";", ":", ",", " ", "!", "?", "-"...
+// Hard version: ignore punctuation: ";", ":", ",", " ", "!", "?", "-"...
+// use string regrex:
