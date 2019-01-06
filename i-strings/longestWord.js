dbec5c92336fa3dd2b4983fe8863e9be08e3a7c9
Sophia Mai <phuongmai001@gmail.com>
2019-01-06 14:56:44 -0800

    longest word exercise

---
+++ i-strings/longestWord.js
@@ -0,0 +1,20 @@
+// return the longest word in a string
+// if there are more than one longest word, put them in a array
+
+function longestWord(str) {
+  // clean input, use regex to clean out special characters
+  const wordArr = str.toLowerCase().match(/[a-z0-9]+/g);
+
+  // sort by word length in descending order
+  const sorted = wordArr.sort((a, b) => b.length - a.length);
+
+  const longestWordArr = sorted.filter(word => word.length === sorted[0].length);
+
+  return (longestWordArr.length === 1) ? longestWordArr[0] : longestWordArr;
+}
+
+const testResult = longestWord('Today is a beautiful day');
+console.log(testResult);
+const test2 = longestWord('Hello there, I love the sun');
+console.log(test2);
+
