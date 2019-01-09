ea8788b6ae8e9c45eb8e94e98bcbfae1de0dab09
Sophia Mai <phuongmai001@gmail.com>
2019-01-09 09:13:02 -0800

    add constructor for trie

--- f-tree/trie.js
+++ f-tree/trie.js
@@ -1,1 +1,24 @@
 // Trie data structure
+
+const Node = (data) => {
+  this.data = data;
+  this.isWord = false;
+  this.children = {};
+  // this.prefixes = 0;
+};
+
+function Trie () {
+  this.root = new Node(''); // initialize tree with empty root node
+}
+
+Trie.prototype.add = function (word) {
+  // step 1: caution check if the root exists
+  if (!this.root) {
+    return null;
+  }
+  this._addNode(this.root, word); // define below
+};
+
+Trie.prototype._addNode = function (rootNode, word) {
+  //
+};
