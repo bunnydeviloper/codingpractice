943c1579385c1d0575f34cfbcaab8701e20eca9d
Sophia Mai <phuongmai001@gmail.com>
2019-01-09 19:38:57 -0800

    add trie remove method

---
+++ f-tree/trie-remove.js
@@ -0,0 +1,13 @@
+const Trie = require('./trie-addAndSearch.js');
+
+console.log('--------------------------------------');
+const myTrie2 = new Trie();
+myTrie2.add('sleep');
+myTrie2.add('see');
+console.log(JSON.stringify(myTrie2, null, 4));
+
+console.log('--------------------------------------');
+
+Trie.prototype.remove = funtion(word) {
+  // remove a word from a trie
+};
