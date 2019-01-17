576da07a4ab0449e859327b03d5c72db5dbc52f1
Sophia Mai <phuongmai001@gmail.com>
2019-01-17 13:05:40 -0800

    add deserializer fn

--- f-tree/serializeDeserialize.js
+++ f-tree/serializeDeserialize.js
@@ -25,7 +25,7 @@ function serialize (tree) {
   else {
     let result = [];
     serializer(tree.root, result);
-    return result.join('.');
+    return result.join(',');
   }
 }
 
@@ -44,8 +44,41 @@ function serializer (node, result) {
   serializer(node.right, result);
 }
 
-const serializeResult = serialize(myTree);
-console.log(serializeResult); // '3, 2, 1, #, #, #, 4, #, #'
+console.log('//////////////// Test Serialize ///////////////');
 
+const serializedResult = serialize(myTree);
+console.log(serializedResult); // '3, 2, 1, #, #, #, 4, #, #'
+console.log('\nTest a single node: ', serialize(rightNode)); // 'Invalid tree'
+
+console.log('\n//////////////// Test Deserialize ///////////////');
 
 // Deserialize a tree: convert string to tree
+function deserialize (string) {
+  if (string.length <= 0) return 'Invalid input string';
+
+  else {
+    let data = string.split(',');
+    let currentIndex = 0;
+
+    function deserializer(data) {
+      // if index is out of bound or if current data array value is '#', exit
+      if (currentIndex > data.length || data[currentIndex] === '#') return null;
+
+      // else make a new node with associated value
+      const node = new Node(data[currentIndex], null, null);
+
+      // increase index and repeat process
+      currentIndex++; // odd index, go left
+      node.left = deserializer(data, currentIndex);
+      currentIndex++; // even index, go right
+      node.right = deserializer(data, currentIndex);
+
+      return node;
+    }
+    return new Tree(deserializer(data));
+  }
+}
+
+const deserializedResult = deserialize(serializedResult);
+console.dir(deserializedResult, { depth: null }); // should output 'myTree'
+console.log('\nTest empty string: ', deserialize('')); // 'Invalid input string'
