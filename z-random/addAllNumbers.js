9f219dba59cfc8a0e8e11eff29da3a8846ca84e3
Sophia Mai <phuongmai001@gmail.com>
2019-01-20 19:03:55 -0800

    update addAllNumebers exercise

--- z-random/addAllNumbers.js
+++ z-random/addAllNumbers.js
@@ -1,10 +1,18 @@
-// return the sum of all arguments (numbers)
+// return the sum of all parameters entered
+// (regardless of the amount of numbers) -- NO ARRAYS
 
-function addAll() {
+function addAll() { // using arguments param
   return Object.values(arguments).reduce((accum, curr) => {
     return accum + curr;
   });
 }
 
-const test = addAll(2, 4, 4, 5, 9, 1);
-console.log(test); // 25
+function addAll2(...numbers) { // using ...rest operator
+  return numbers.reduce((a, c) => a + c);
+}
+
+console.log(addAll(2, 4, 4, 5, 9, 1)); // 25
+console.log(addAll2(2, 4, 4, 5, 9, 1)); // 25
+
+const test2 = addAll(1, 2, 3, 4, 5, 6, 7, 8, 9, 10);
+console.log(test2); // 55
