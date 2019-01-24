44731cfed340054099750d628918b5149a8b8558
Sophia Mai <phuongmai001@gmail.com>
2019-01-23 20:20:08 -0800

    add another way

--- h-arraysObjects/largestSubArraySum.js
+++ h-arraysObjects/largestSubArraySum.js
@@ -20,3 +20,24 @@ result = largestSubArraySum([5, -6, 4, -3, 3, 3]);
 console.log('Expect result to be 7: ', result);
 result = largestSubArraySum([5, 6, 4, 3, -7, 1]);
 console.log('Expect result to be 18: ', result);
+
+function largestSubArraySum2 (array) {
+    if (array.length < 1) return null;
+
+    let maxSum = 0;
+    let currMaxSum = 0;
+
+    for (let num of array) {
+        currMaxSum += num;
+        if (currMaxSum < 0) currMaxSum = 0;
+        maxSum = Math.max(currMaxSum, maxSum);
+    }
+    return maxSum;
+}
+
+result = largestSubArraySum2([-2, -3, 4, -1, -2, 1, 5, -3]);
+console.log('Expect result to be 7: ', result);
+result = largestSubArraySum2([5, -6, 4, -3, 3, 3]);
+console.log('Expect result to be 7: ', result);
+result = largestSubArraySum2([5, 6, 4, 3, -7, 1]);
+console.log('Expect result to be 18: ', result);
