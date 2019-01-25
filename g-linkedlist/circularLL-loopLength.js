f16d4950232f57e0a5c2d4268e3f3cee7043007a
Sophia Mai <phuongmai001@gmail.com>
2019-01-24 21:44:09 -0800

    add loop length linked list exercise

---
+++ g-linkedlist/circularLL-loopLength.js
@@ -0,0 +1,47 @@
+// given a circular link list, find the length of the circular loop
+
+function loopLength (list) {
+  if (!list.head) return null;
+
+  let p1 = list.head;
+  let p2 = list.head;
+  let isLoop = false;
+
+  while (p2.next.next) {
+    p1 = p1.next;      // slow pointer
+    p2 = p2.next.next; // fast pointer, will move twice as fast
+
+    // when p1 meets p2, we found a cycle and break out of the loop
+    if (p1.value === p2.value) {
+      isLoop = true;
+      break;
+    }
+  }
+
+  // if there is a loop, we find the length by increasing fast pointer 1 step at a time
+  // (keep slow pointer at the same fixed location)
+  // until fast = slow then that means we walked the entire circle
+  let length = 0;
+  if (isLoop) {
+    length++;
+    p2 = p2.next;
+    while (p1.value !== p2.value) {
+      length++;
+      p2 = p2.next;
+    }
+  }
+
+  return length;
+}
+
+const f = { value: 6, next: null };
+const e = { value: 5, next: f };
+const d = { value: 4, next: e };
+const c = { value: 3, next: d };
+const b = { value: 2, next: c };
+const a = { value: 1, next: b };
+f.next = c;
+const myList = { head: a };
+
+const resultLength = loopLength(myList);
+console.log('Expect loop length to be 4 ', resultLength);
