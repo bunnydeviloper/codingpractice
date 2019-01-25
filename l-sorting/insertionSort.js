2d91fa23391c010588f9cbb12ba9a2830d57eeab
Sophia Mai <phuongmai001@gmail.com>
2019-01-24 23:58:55 -0800

    add comment for insertion sort

--- l-sorting/insertionSort.js
+++ l-sorting/insertionSort.js
@@ -1,4 +1,7 @@
 // insertion sort: find the smallest, add to list, repeat
+// imagine card game, you're going through the array one at a time
+// each iteration removes 1 elem from array & 'insert' it to right location
+// repeat until no input elements remain
 
 function insertionSort(array) {
   for (let i = 0; i < array.length; i++) {
@@ -17,4 +20,4 @@ const test = insertionSort([2, 3, 1, 4, 5, 8, 7, 6]);
 console.log(test);
 
 const list = [54, 26, 93, 17, 77, 31, 44, 55, 20]
-console.log(insertionSort(list)) // [ 17, 20, 26, 31, 44, 54, 55, 77, 93 ]
+console.log(insertonSort(list)) // [ 17, 20, 26, 31, 44, 54, 55, 77, 93 ]
