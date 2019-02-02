39c427ef1244d25a2ed64dc684ad30a312e7a623
Sophia Mai <phuongmai001@gmail.com>
2019-02-02 13:27:31 -0800

    two sum with object

--- h-arraysObjects/twoSumPractice.js
+++ h-arraysObjects/twoSumPractice.js
@@ -9,9 +9,28 @@ function twoSum(array, n) {
   return array;
 }
 
+function twoSumWithObject (array, n) {
+  const hashTable = {};
+
+  for (let i = 0; i < array.length; i++) {
+    const currentValue = array[i];
+    const potentialMatch = n - currentValue;
+    if (!hashTable[potentialMatch]) {
+      hashTable[potentialMatch] = true;
+    } else {
+      return [currentValue, potentialMatch].sort((a, b) => b - a);
+    }
+  }
+
+  return [];
+}
+
 const testInput = [3, 5, -4, 8, 11, 1, -1, 6]
 const testResult = twoSum(testInput, 10);
 console.log(testResult);
+
+const testResultWithObject = twoSumWithObject(testInput, 10);
+console.log(testResultWithObject);
 
 // TIME: O(nlog(n)) | SPACE: O(1)
 const twoSumWithSortedArray = (array, n) => {
