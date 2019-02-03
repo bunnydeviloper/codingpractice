6fa9b7fb7f0b82e6ab6d42f8a3416e594a34e373
Sophia Mai <phuongmai001@gmail.com>
2019-02-03 11:43:22 -0800

    starting mergeksorted fn

--- h-arraysObjects/mergeKSortedArray.js
+++ h-arraysObjects/mergeKSortedArray.js
@@ -13,9 +13,25 @@
 // NK is total # of elems across all array
 // log(K) is maximum # of bubble down operations per elem (heap height)
 
-// S1: create a min heap with the first elem from each array
-// (total K elem) to track the smallest number
-// NOTE: store which_array and what_index the elem came from
+function mergeKSorted (arrays) {
+  let result = [];
+  let minHeap = [];
+
+  // S1: create a min heap with the first elem from each array
+  // (total K elem) to track the smallest number
+  // NOTE: store which_array and what_index the elem came from
+
+  return result;
+}
+
+let testInput = [
+  [5, 6, 8, 10],
+  [3, 7, 12, 13],
+  [1, 10, 11, 15],
+  [2, 4, 9, 14],
+];
+console.log(mergeKSorted(testInput));
+// [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16]
 
 // S2: remove the smallest element from heap (top value)
 
