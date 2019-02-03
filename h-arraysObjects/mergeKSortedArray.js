60f7f4a274982a638616c712eae6877c7f2cd32b
Sophia Mai <phuongmai001@gmail.com>
2019-02-03 13:22:06 -0800

    remove comment for later

--- h-arraysObjects/mergeKSortedArray.js
+++ h-arraysObjects/mergeKSortedArray.js
@@ -33,20 +33,3 @@ let testInput = [
 console.log(mergeKSorted(testInput));
 // [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16]
 
-// S2: remove the smallest element from heap (top value)
-
-// S3: increment the elem_index and substitute value at the top
-// for the next value in that array
-
-// S4: Bubble down the top value until it finds the correct level
-
-
-// S5: Repeat S2 - S4 until you reach the end of ONE of the array
-
-// S6: At this point, substitute the value at the top for INFINITY
-// NOTE: Infinity will always bubble all the way down
-
-// S7: perform S6 anytime an array runs out of values
-// When the top elem of the heap is INFINITY, you're done
-// all the numbers are added to the result array
-
