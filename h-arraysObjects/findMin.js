dd3104d45fc6de3d9fdd1934b737e09dcaa02445
Sophia Mai <phuongmai001@gmail.com>
2019-02-02 22:50:37 -0800

    add findMin array fn

---
+++ h-arraysObjects/findMin.js
@@ -0,0 +1,19 @@
+const testInput = [1, 0, -4, 5, 6, 10, 12]; // unsorted arr
+
+// TIME: O(n), SPACE: O(1)
+function findMin (array) {
+  if (array.length < 1) return null;
+  if (array.length === 1) return array[0];
+
+  let min = array[0];
+  for (let i = 0; i < array.length; i++) {
+    if (array[i] < min) {
+      min = array[i];
+    }
+  }
+
+  return min;
+}
+
+const testResult = findMin(testInput);
+console.log(testResult); // -4
