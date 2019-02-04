d2b25d63d6b9348313e94737329058b626a0d40b
Sophia Mai <phuongmai001@gmail.com>
2019-02-03 23:24:28 -0800

    add insert method with interative solution

--- bstConstructionIterative.js
+++ bstConstructionIterative.js
@@ -5,7 +5,29 @@ class BST {
     this.right = null;
   }
 
+  // AVERAGE: O(log(n)) time | O(1) space
+  // WORST: O(n) time | O(1) space (b/c not using call stack recursion)
   insert(value) {
+    let currentNode = this;
+    while (true) {
+      if (value < currentNode.value) {
+        if (currentNode.left !== null) {
+          currentNode = currentNode.left;
+        } else {
+          currentNode.left = new BST(value);
+          break;
+        }
+      } else if (value > currentNode.value) {
+        if (currentNode.right !== null) {
+          currentNode = currentNode.right;
+        } else {
+          currentNode.right = new BST(value);
+          break;
+        }
+      } else { // value === currentNode.value
+        break; // no add duplicate value
+      }
+    }
     return this;
   }
 
@@ -20,7 +42,7 @@ class BST {
 
 const myTree = new BST(10);
 myTree.insert(5).insert(2).insert(10).insert(1); // tree w/ only left br
-console.log(myTree, { depth: null });
+console.dir(myTree, { depth: null });
 
-myTree.remove(2).remove(1).remove(5);
-myTree.insert(12).insert(14).insert(5); // tree w/ only right br
+// myTree.remove(2).remove(1).remove(5);
+// myTree.insert(12).insert(14).insert(5); // tree w/ only right br
