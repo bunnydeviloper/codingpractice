a10983c498a607a0b1701bee416d5a69d3e856da
Sophia Mai <phuongmai001@gmail.com>
2019-02-04 11:35:44 -0800

    set up for doubly ll constructor

---
+++ g-linkedlist/dllConstruction.js
@@ -0,0 +1,49 @@
+// write a class for a doubly linked list
+
+class Node {
+  constructor(value) {
+    this.value = value;
+    this.prev = null;
+    this.next = null;
+  }
+}
+
+
+class DoublyLinkedList {
+  constructor() {
+    this.head = null;
+    this.tail = null;
+  }
+
+  setHead(node) {
+    //
+  }
+
+  setTail(node) {
+    //
+  }
+
+  insertBefore(node, nodeToInsert) {
+    //
+  }
+
+  insertAfter(node, nodeToInsert) {
+    //
+  }
+
+  insertAtPosition(position, nodeToInsert) {
+    //
+  }
+
+  removeNodesWithValue(value) {
+    //
+  }
+
+  remove(node) {
+    //
+  }
+
+  containsNodeWithValue(value) {
+    //
+  }
+}
