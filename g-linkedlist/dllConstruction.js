adb0c068c3110d456e0764a3d72852d918f0a8ec
Sophia Mai <phuongmai001@gmail.com>
2019-02-04 12:19:03 -0800

    add contains method

--- g-linkedlist/dllConstruction.js
+++ g-linkedlist/dllConstruction.js
@@ -43,11 +43,22 @@ class DoublyLinkedList {
   }
 
   setHead(node) {
-    //
+    if (this.head === null) { // empty ll
+      this.head = node;
+      this.tail = node;
+    } else {
+      insertBefore(this.head, node);
+    }
+    return this;
   }
 
   setTail(node) {
-    //
+    if (this.tail === null) { // empty ll
+      this.tail = node;
+      this.head = node;
+    } else {
+      insertAfter(this.tail, node);
+    }
   }
 
   insertBefore(node, nodeToInsert) {
@@ -71,6 +82,34 @@ class DoublyLinkedList {
   }
 
   containsNodeWithValue(value) {
-    //
+    let node = this.head;
+    while (node !== null && node.value !== value) {
+      node = node.next;
+    }
+    return (node !== null);
   }
 }
+
+const emptyLL = new DoublyLinkedList();
+console.log(emptyLL);
+
+// Set up a simple LL: null <-> 1 <-> 2 <-> 3 <-> 4 <-> 5 <-> null
+const five = new Node(5);
+const four = new Node(4);
+const three = new Node(3);
+const two = new Node(2);
+const one = new Node(1);
+
+one.prev = null; one.next = two;
+two.prev = one; two.next = three;
+three.prev = two; three.next = four;
+four.prev = three; four.next = five;
+five.prev = four; five.next = null;
+
+const simpleLL = new DoublyLinkedList();
+simpleLL.head = one; simpleLL.tail = five;
+// console.dir(simpleLL, { depth: null });
+
+// ---------- TESTS ------------
+console.log(simpleLL.containsNodeWithValue(8)); // false
+console.log(simpleLL.containsNodeWithValue(5)); // true
