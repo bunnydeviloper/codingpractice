1caf0b7c8f73c5ed7b786597b0f8fcbe9fef08e2
Sophia Mai <phuongmai001@gmail.com>
2019-02-03 21:59:47 -0800

    update insert bst fn

--- f-tree/bstConstruction.js
+++ f-tree/bstConstruction.js
@@ -6,16 +6,27 @@ class BST {
   }
 
   insert(value) {
-    if (value > this.value) {
-      //
-    } else if (value < this.value) {
-      //
+    if (value < this.value) {
+      if (this.left === null) {
+        this.left = new BST(value);
+      } else {
+        this.left.insert(value);
+      }
+    } else if (value > this.value) {
+      if (this.right === null) {
+        this.right = new BST(value);
+      } else {
+        this.right.insert(value);
+      }
+    } else { // when value === this.value
+      return this;
     }
     return this;
   }
 
   contains(value) {
     //
+    return true/false;
   }
 
   remove(value) {
@@ -24,5 +35,5 @@ class BST {
 }
 
 const myTree = new BST(10);
-myTree.insert(2);
-console.log(myTree);
+myTree.insert(10).insert(5).insert(15).insert(5).insert(2).insert(14).insert(22);
+console.dir(myTree, { depth: null });
