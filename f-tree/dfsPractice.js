9f6e0ea06af2f11f1a0a661cbe734928e2a597f3
Sophia Mai <phuongmai001@gmail.com>
2019-02-18 20:49:32 -0800

    add dfsPractice

---
+++ f-tree/dfsPractice.js
@@ -0,0 +1,18 @@
+function depthFirstSearch(node, array = []) {
+  if (!node) return array;
+
+  array.push(node.name);
+  for (const child of node.children) {
+    depthFirstSearch(child, array);
+  }
+  return array;
+}
+
+const f = {name: 'F', children: [{name: 'I', children: []}, {name: 'J', children: []}]};
+const b = {name: 'B', children: [{name: 'E', children: []}, f]};
+const d = {name: 'D', children: [{name: 'G', children: [{name: 'K', children: []}]}, {name: 'H', children: []}]};
+const a = {name: 'A', children: [b, {name: 'C', children: []}, d]};
+console.dir(a, { depth: null });
+
+const result = depthFirstSearch(a);
+console.log(result);
