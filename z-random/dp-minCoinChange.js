0a3e03c3b72dbf69c5a56b8004b2cfedca756ee7
Sophia Mai <phuongmai001@gmail.com>
2019-03-01 00:11:54 -0800

    add minimun number of coins for change solution

---
+++ z-random/dp-minCoinChange.js
@@ -0,0 +1,41 @@
+function minNumberOfCoinsForChange(n, demons) {
+	console.log(minNumberOfCoinsForChange2(24, [1, 2, 4, 9]));
+	return (minNumberOfCoinsForChange2(n, demons)); 
+}
+function minNumberOfCoinsForChange2(n, denoms) {
+  // Write your code here.
+	if (n === 0) return 0;
+	
+	let remain = n;
+	console.log('outside', remain);
+	let totalCoins = 0;
+	let sorted = denoms.sort((a, b) => a - b);
+	let i = sorted.length - 1;
+	let temp = Infinity;
+	
+	while (remain > 0 && i >= 0) {
+		console.log('begin', totalCoins);
+		if (n % sorted[i] === 0) {
+			temp = Math.min(temp, Math.floor(n / sorted[i]));
+		}
+		if (remain % sorted[i] === 0) {
+			console.log('here', totalCoins);
+			totalCoins += Math.floor(remain / sorted[i]);
+			remain = remain % sorted[i];
+		} else {
+			if (remain > sorted[i]) {
+				totalCoins += Math.floor(remain / sorted[i]);
+				remain = remain % sorted[i];
+			}
+		}
+		i--;
+	}
+	
+	const final = (totalCoins === 0) ? -1 : totalCoins;
+	
+	const tempAgain = (temp === Infinity) ? -1 : temp;
+	
+	const result = (remain > 0) ? tempAgain : final;
+	return result;
+}
+
